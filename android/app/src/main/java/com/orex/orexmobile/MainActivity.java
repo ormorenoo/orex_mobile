51289bf22b7d4ce07994ac40ffdcde6616338983
ormorenoo <55900051+ormorenoo@users.noreply.github.com>
2025-12-17 11:42:29 -0500

    debugger available

--- android/app/src/main/java/com/orex/orexmobile/MainActivity.java
+++ android/app/src/main/java/com/orex/orexmobile/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.orex.orexmobile;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.orex.orexmobile',
   appName: 'orexIonic',
   webDir: 'www',
   bundledWebRuntime: false,
